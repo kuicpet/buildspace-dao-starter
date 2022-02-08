2492e3636842e4558eb88124ff9234e76b463765
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2022-02-08 11:54:08 +0100

    ft--custom token deployed

---
+++ scripts/5-deploy-token.js
@@ -0,0 +1,22 @@
+import sdk from './1-initialize-sdk.js'
+
+// insert app address
+const app = sdk.getAppModule('0xc521f33CCaCA5a62C19D99474aBaDF91D73cEB39')
+
+;(async () => {
+  try {
+    // Deploy a standard ERC-20 contract.
+    const tokenModule = await app.deployTokenModule({
+      // Token name
+      name: 'CreatorsDAO Governance Token',
+      // Token symbol
+      symbol: 'CREA',
+    })
+    console.log(
+      'Successfully deployed token module, address',
+      tokenModule.address
+    )
+  } catch (error) {
+    console.log('Failed to deploy token module', error)
+  }
+})()
